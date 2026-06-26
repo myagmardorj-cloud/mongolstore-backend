c4a4e9a47fceb8cbd512eee2c999ff87270609cf
MYAGMARDORJ.N <myagmardorj@gmail.com>
2026-06-26 14:25:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def health():
     return jsonify({'status': 'ok', 'service': 'MongolStore Backend'})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=10000)
